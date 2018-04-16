43ab92ba0a5a4c81a07817f0e4545ae6b7745609
FrostedWeFall <Thevirtualvirgo@gmail.com>
2018-04-17 00:43:15 +0100

    Mini recode 1

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
-const bot = new Discord.Client({disableEveryone: true});
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} has been successfully loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
 
 bot.on("ready", async () => {
 
@@ -10,154 +28,37 @@ bot.on("ready", async () => {
 
 });
 
+bot.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} | (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  bot.user.setActivity(`over ${bot.guilds.size} guilds! | ~help  | Watching ${bot.users.size} players!`, {type: "WATCHING"});
+});
 
-bot.on("message", async message => {
-   if(message.author.bot) return;
-   if(message.channel.type === "dm") return;
-
-   let prefix  = botconfig.prefix;
-   let messageArray = message.content.split(" ");
-   let cmd = messageArray[0];
-   let args = messageArray.slice(1);
-
-   if(cmd === `${prefix}creator`){
-
-    return message.channel.send("FrostedWeFall is my creator :wink:");
-   }
-
-   if(cmd === `${prefix}donate`){
-
-    return message.channel.send("Please be sure to donate to help support development of FrostBot **Donation link will be shown here**");
-  }
-
-   if(cmd === `${prefix}ping`) {
-      let pingembed = new Discord.RichEmbed()
-      .setDescription("🎉")
-      .setColor("#42f4e5")
-      .addField(`${Date.now() - (message.createdTimestamp)}ms`, "Roundtrip and Response ↪")
-      .addField(`${Math.round(bot.ping)}ms`, "API ping 🏓");
-      
-      return message.channel.send(pingembed);
-        
-    }
-
-   if(cmd === `${prefix}info`){
-
-
-    let infoembed = new Discord.RichEmbed()
-    .setTitle("FrostBot Information")
-    .setDescription("Here is some information about me! ^-^")
-    .setColor("#42f4e5")
-    .addField("Bot Name", bot.user.username)
-    .addField("Bot Creator", botconfig.author)
-    .addField("Bot Version", botconfig.version)
-    .addField(`${Date.now() - (message.createdTimestamp)}ms`, "Roundtrip and Response ↪")
-    .addField(`${Math.round(bot.ping)}ms`, "API ping 🏓");     
-
-    return message.channel.send(infoembed);
-
-   }
-
-    if(cmd === `${prefix}help`){
-      let botembed = new Discord.RichEmbed()
-      .setTitle("FrostBot Help Commands")
-      .setDescription("Need some help with FrostBot?")
-      .setColor("#42f4e5")
-      .addField("~help", "Sends this message ^-^")
-      .addField("~ping", "Shows the clientside and roundtrip ping")
-      .addField("~info", "Gives you some Information about the bot")
-      .addField("~guildinfo", "Gives you some Information about the current Guild")
-      .addField("~donate", "Donate to help support development of FrostBot")
-      .addField("~report [<user>] [<reason>]", "Reports a player")
-      .addField("~kick [<user>] [<reason>]", "Kicks a player from the guild")
-      .addField("~creator", "Whos the creator of Frost?");    
-
-      return message.channel.send(botembed);
-
-    }
-
-   if(cmd === `${prefix}guildinfo`){
-
-    let sicon = message.guild.iconURL;
-    let serverinfoembed = new Discord.RichEmbed()
-    .setTitle("Server Information")
-    .setDescription("Some information about the current guild!")
-    .setThumbnail(sicon)
-    .setColor("#42f4e5")    
-    .addField("Guild Name", message.guild.name)
-    .addField("Guild Member Count", message.guild.memberCount)
-    .addField("Guild Created On", message.guild.createdAt)
-    .addField("You Joined this Guild at", message.member.joinedAt);
-
-    return message.channel.send(serverinfoembed);
-
-}
-
-  if(cmd === `${prefix}report`){
-
-    const rUser = await message.guild.fetchMember(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("Invalid user");
-    let reason  = args.join(" ").slice(22);
-    if(!reason) return message.channel.send("You must specify a reason to report this player")
-    let reportembed = new Discord.RichEmbed()
-    .setDescription("Report A User")
-    .setColor("#42f4e5")
-    .addField("Reported User", `${rUser}`)
-    .addField("Reported By", `${message.author}`)
-    .addField("Channel", message.channel)
-    .addField("TimeStamp", message.createdAt)
-    .addField("Reason", reason);
-
-    let reportschannel = message.guild.channels.find(`name`, "reports");
-    if(!reportschannel) return message.channel.send("Couldn't find appropriate reports channel!");
-
-    message.delete().catch(O_o=>{});
-    reportschannel.send(reportembed);
-  }
-   if(cmd === `${prefix}kick`){
-
-      message.delete().catch(O_o=>{});
-      const kUser = await message.guild.fetchMember(message.mentions.users.first() || message.guild.members.get(args[0]));
-
-      
-      
-      let kickreason  = args.join(" ").slice(22);
-      if(!kUser) return message.channel.send("You must specify a user!");
-      if(!kUser.kickable) return message.channel.send("❌ The bot has no permissions to kick this player");
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("🤔 You do not have the required permissions to kick this user!");
-      if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("❌ That person can't be kicked!");
-      if(!kickreason) return message.channel.send("🤔 You must specify a reason to kick this player");
-
-
-      message.channel.send(`✅ ${kUser} has been kicked from the guild for ${kickreason}`);
-      message.guild.member(kUser).kick(kickreason);
-   }
-
-
-   if(cmd === `${prefix}warn`){
-
-      message.delete().catch(O_o=>{});
-      const wUser = await message.guild.fetchMember(message.mentions.users.first() || message.guild.members.get(args[0]));
-
-      
-      
-      let warnreason  = args.join(" ").slice(22);
-      if(!wUser) return message.channel.send("🤔 You must specify a user!");
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("🤔 You do not have the required permissions to warn this user!");
-      if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("❌ That person can't be warned!");
-      if(!warnreason) return message.channel.send("🤔 You must specify a reason to warn this player");
-
-
-      message.channel.send(`✅ ${wUser} has been warned for ${warnreason}`);
-      message.guild.member(wUser).warn(warnreason);
-    }
+bot.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  bot.user.setActivity(`over ${bot.guilds.size} guilds! | ~help  | Watching ${bot.users.size} players!`, {type: "WATCHING"});
 });
 
 
-  
+bot.on("message", async message => {
+
+  if (message.author.bot || message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+
+
+  let prefix = prefixes[message.guild.id].prefixes;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+});
 
 bot.login(botconfig.token);
-
-
-
-
