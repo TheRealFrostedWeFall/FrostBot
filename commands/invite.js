609f1b312debd422f1a3e0972fa7f632afa5b49c
FrostedWeFall <Thevirtualvirgo@gmail.com>
2018-05-12 15:52:07 +0100

    Changes to invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,10 +6,10 @@ module.exports.run = async (bot, message) => {
     .setDescription("Adopt Frost!")
     .setThumbnail(bicon)
     .setColor("#0263ff")
-    .addField("Adopt Frost", `Click the link below`)
-    .addField("https://discordapp.com/oauth2/authorize?client_id=434159357434003456&permissions=268528711&scope=bot", `Be sure to vote for the bot too`)
-    .addField("Vote Link Below", "https://discordbots.org/bot/434159357434003456/vote")
-    .addField("FrostBot Support Discord", "https://discord.gg/DuKqq5r")
+    .addField("Adopt Frost", `Click the link below`, true)
+    .addField("https://discordapp.com/oauth2/authorize?client_id=434159357434003456&permissions=268528711&scope=bot", `Be sure to vote for the bot too`, true)
+    .addField("Vote Link Below", "https://discordbots.org/bot/434159357434003456/vote", true)
+    .addField("FrostBot Support Discord", "https://discord.gg/DuKqq5r", true)
     .setFooter(`Requested By ${message.author.username} ID: ${message.author.id}`, message.author.displayAvatarURL);
   
   message.channel.send(inviteembed);
